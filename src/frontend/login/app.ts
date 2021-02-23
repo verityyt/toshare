64067d79feb06540ee93a208f903f7b2ea3dfe82
Verity <joshua-schmidt@online.de>
2021-02-23 22:39:37 +0100

    :sparkles: Add login page

---
+++ src/frontend/login/app.ts
@@ -0,0 +1,18 @@
+const list = document.getElementById("list") as HTMLUListElement
+
+for (const listItem of list.children) {
+    const title = listItem.getElementsByClassName("todo-title").item(0) as HTMLParagraphElement
+    const button = listItem.getElementsByClassName("todo-button").item(0) as HTMLInputElement
+
+    button.addEventListener("click", () => {
+
+        if (button.value == "Done") {
+            title.classList.add("done")
+            button.value = "Remove"
+        } else if (button.value == "Remove") {
+            list.removeChild(listItem)
+        }
+
+    })
+
+}
