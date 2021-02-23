06e12033874fab87fe38da31ab4416e992d41ca7
Verity <joshua-schmidt@online.de>
2021-02-23 22:57:39 +0100

    :lipstick: Rework design of frontend + add register button

--- src/frontend/register/app.ts
+++ src/frontend/register/app.ts
@@ -1,1 +1,15 @@
 console.log("Opened page")
+
+let username = document.getElementById("tf-username") as HTMLInputElement
+let password = document.getElementById("tf-password") as HTMLInputElement
+let register = document.getElementById("button") as HTMLInputElement
+
+register.addEventListener("click", () => {
+    let error = document.getElementById("error") as HTMLParagraphElement
+
+    if(username.value == "" || password.value == "") {
+        error.textContent = "Invalid username or password!"
+    }else {
+        error.textContent = ""
+    }
+})
