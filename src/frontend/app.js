9f94734da2bc89ddc5b029a759e4bc13314b8c80
Verity <joshua-schmidt@online.de>
2021-02-23 18:54:37 +0100

    :construction: Introduce loop through all todos

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,1 +1,8 @@
-console.log("Hello");
+const list = document.getElementById("list");
+for (const listItem of list.children) {
+    for (const child of listItem.children) {
+        if (child.classList.contains("todo-title")) {
+            console.log(`Test: ${child.textContent}`);
+        }
+    }
+}
--- src/frontend/app.ts
+++ src/frontend/app.ts
@@ -1,1 +1,9 @@
-console.log("Hello")
+const list = document.getElementById("list") as HTMLUListElement
+
+for (const listItem of list.children) {
+    for (const child of listItem.children) {
+        if(child.classList.contains("todo-title")) {
+            console.log(`Test: ${child.textContent}`)
+        }
+    }
+}
