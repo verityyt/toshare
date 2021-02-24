0c2fc120041b9efc72b540018aaed12870d667d2
Verity <joshua-schmidt@online.de>
2021-02-24 21:45:29 +0100

    :bug: Update request so username and password are send as header not as query (no longer in the browser history)

--- src/register/app.ts
+++ src/register/app.ts
@@ -14,15 +14,21 @@ register.addEventListener("click", () => {
     } else {
         error.textContent = ""
 
-        window.fetch(`${url}/register?username=${username.value}&password=${password.value}`, {
-            method: 'GET',
+        window.fetch(`${url}/register`, {
+            method: 'POST',
             mode: 'cors',
             cache: 'no-cache',
             credentials: 'include',
             headers: {
                 "Access-Control-Allow-Origin": "https://inceptioncloud.net/toshare/",
-                "Access-Control-Allow-Credentials": "true"
-            }
+                "Access-Control-Allow-Credentials": "true",
+                "username": username.value,
+                "password": password.value
+
+            },
+            body: JSON.stringify({
+
+            })
         }).then(response => response.json()).then(data => {
             window.location.assign(data.redirect)
         })
