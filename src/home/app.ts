b0175f3b3e0e53a49d1cd578442f07cca532acce
Verity <joshua-schmidt@online.de>
2021-02-26 17:30:32 +0100

    :sparkles: Finish read-request -> Display and init all fetched todos

--- src/home/app.ts
+++ src/home/app.ts
@@ -12,26 +12,63 @@ fetch(`${url2}/read`, {
         "Access-Control-Allow-Credentials": "true"
     }
 }).then(response => response.json()).then(data => {
+    if (data.redirect != null) {
+        window.location.assign(data.redirect)
+    } else {
+        for (const todo of data) {
+            const item = document.createElement("li")
+
+            const paragraph = document.createElement("p")
+            paragraph.classList.add("todo-title")
+            paragraph.textContent = todo.todo
+
+            const button = document.createElement("input")
+            button.classList.add("todo-button")
+            button.type = "button"
+            if (todo.status == "open") {
+                button.value = "Done"
+            } else if (todo.status == "done") {
+                button.value = "Remove"
+            }
+
+            item.appendChild(paragraph)
+            item.appendChild(button)
+
+            list.appendChild(item)
+        }
+
+        for (const listItem of list.children) {
+            const title = listItem.getElementsByClassName("todo-title").item(0) as HTMLParagraphElement
+            const button = listItem.getElementsByClassName("todo-button").item(0) as HTMLInputElement
+
+            button.addEventListener("click", () => {
+
+                if (button.value == "Done") {
+                    title.classList.add("done")
+                    button.value = "Remove"
+                } else if (button.value == "Remove") {
+                    list.removeChild(listItem)
+                }
+
+            })
+        }
+    }
+})
+
+/*fetch(`${url2}/add`, {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'include',
+    headers: {
+        "Access-Control-Allow-Origin": "https://inceptioncloud.net/toshare/",
+        "Access-Control-Allow-Credentials": "true",
+        "todo": "Help mom"
+    }
+}).then(response => response.json()).then(data => {
 
     if(data.redirect != null) {
         window.location.assign(data.redirect)
     }
 
-})
-
-for (const listItem of list.children) {
-    const title = listItem.getElementsByClassName("todo-title").item(0) as HTMLParagraphElement
-    const button = listItem.getElementsByClassName("todo-button").item(0) as HTMLInputElement
-
-    button.addEventListener("click", () => {
-
-        if (button.value == "Done") {
-            title.classList.add("done")
-            button.value = "Remove"
-        } else if (button.value == "Remove") {
-            list.removeChild(listItem)
-        }
-
-    })
-
-}
+})*/
